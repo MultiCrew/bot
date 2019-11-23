66be7b90087a45526070abdd86cdd26569cce152
Harry Cameron <hcphoon01@gmail.com>
2019-11-23 13:55:55 +0000

    Added mysql support

---
+++ commands/search.js
@@ -0,0 +1,9 @@
+const db = require('../db');
+
+module.exports = {
+    name: 'search',
+    description: 'Search all flights',
+    execute(message, args) {
+        
+    }
+}
--- index.js
+++ index.js
@@ -22,7 +22,7 @@ client.once('ready', () => {
 
 client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
-    
+
     if (message.channel.id != config.get('discord.channel')) return;
 
 	const args = message.content.slice(prefix.length).split(/ +/);
