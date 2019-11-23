8eadf080ea2f065b315470969666e5f9d2a38a16
Harry Cameron <hcphoon01@gmail.com>
2019-11-23 16:18:45 +0000

    added initial search command

--- commands/search.js
+++ commands/search.js
@@ -1,9 +1,29 @@
 const db = require('../db');
+const AsciiTable = require('ascii-table')
+let resultList = [];
 
 module.exports = {
     name: 'search',
     description: 'Search all flights',
     execute(message, args) {
-        
+        if (args.length == 0) {
+            db.query('SELECT * FROM flight_requests', function(err, res) {
+                if (err) throw err;
+                res.forEach(result => {
+                   resultList.push(result);
+                });
+                const fTable = createTable(resultList);
+                message.reply("```" + fTable.toString() + "```");
+            })
+        }
     }
+}
+
+function createTable(data) {
+    const table = new AsciiTable('Search Results');
+    table.setHeading('ID', 'Departure', 'Arrival', 'Aircraft');
+    data.forEach(item => {
+        table.addRow(item.id, item.departure, item.arrival, item.aircraft)
+    })
+    return table;
 }
