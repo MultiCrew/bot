6e405bfb5412a44a15f988cbf4e45e30f1e0770e
Harry Cameron <hcphoon01@gmail.com>
2019-11-24 21:08:05 +0000

    added initial todo command plus comments

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,7 @@ module.exports = {
 	execute(message, args) {
 		const data = [];
 		const { commands } = message.client;
-
+		
 		if (!args.length) {
 			data.push('Here\'s a list of all my commands:');
 			data.push(commands.map(command => command.name).join(', '));
---
+++ commands/todo.js
@@ -0,0 +1,76 @@
+const Octokit = require("@octokit/rest");
+const Discord = require('discord.js');
+const AsciiTable = require('ascii-table');
+const config = require('config');
+const octokit = Octokit({
+    auth: config.get('github.token'),
+    userAgent: 'MultiCrew Bot'
+});
+
+module.exports = {
+    name: 'todo',
+    description: 'Interact with GitHub Projects',
+    help: false,
+    execute(message, args) {
+        if(args.length) {
+            switch(args[0]) {
+                case 'add':
+                    addItem(message, args)
+            }
+        }
+    }
+}
+
+// Adds a card to the To Do column of the project
+function addItem(message, args) {
+    const mtc = message.guild.emojis.find(emoji => emoji.name === "mtc");
+    var project = 0;
+    var embed = new Discord.RichEmbed()
+        .setTitle('Create To-Do Card')
+        .setDescription(`React to this message with ${mtc} to create a card on the copilot project or \:robot: to create a card on the bot project`)
+    message.channel.send(embed).then(function (msg) {
+        msg.react(mtc).then(() => msg.react('🤖'))
+        const filter = (reaction, user) => {
+            return user.id === message.author.id};
+        msg.awaitReactions(filter, {max: 1, time: 10000})
+            .then(collected => {
+                const reaction = collected.first();
+                switch (reaction.emoji.name) {
+                    case 'mtc':
+                        project = 'Copilot';
+                        break;
+                    case '🤖':
+                        project = 'Bot';
+                        break;
+                }
+                embed = new Discord.RichEmbed()
+                    .setTitle('Create To-Do Card')
+                    .setDescription('You have selected to add a card in the ' + project + ' project. You now have 30 seconds to input your card content.');
+                msg.channel.send(embed).then(() => {
+                    const filter = response => {
+                        return response.author.id === message.author.id
+                    }
+                    msg.channel.awaitMessages(filter, {max: 1, time: 30000})
+                        .then(collected => {
+                            if (project == 'Copilot') {
+                            octokit.projects.createCard({
+                                column_id: 7168504,
+                                note: collected.first().content
+                            })
+                            } else if (project == 'Bot') {
+                                octokit.projects.createCard({
+                                    column_id: 7227594,
+                                    note: collected.first().content
+                                })
+                            }
+                            embed = new Discord.RichEmbed()
+                            .setTitle('Create To-Do Card')
+                            .setDescription('A card has been created under the ' + project + ' project, it contained the following information: ```' + collected.first().content + '```');
+                            message.channel.send(embed);
+                        })
+                })
+            })
+            .catch(console.error);
+    });
+
+}
