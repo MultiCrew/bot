2e483b6e469774ddd1ade50f5d7b7e2c44272349
Harry Cameron <hcphoon01@gmail.com>
2019-11-29 19:38:26 +0000

    changed help command

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,40 @@
 const config = require('config');
 const prefix = config.get('discord.prefix');
+const AsciiTable = require('ascii-table');
 
 module.exports = {
+	name: 'help',
+	description: 'List all of my commands or info about a specific command.',
+	aliases: ['commands'],
+	usage: '[command name]',
+	cooldown: 5,
+	execute(message, args) {
+		const {commands} = message.client;
+
+		if(!args.length) {
+			const table = new AsciiTable('Help Commands');
+			table.setHeading('Name', 'Usage')
+			commands.map(command => {
+				table.addRow(command.name, command.usage);
+			})
+			return message.channel.send('```' + table.toString() + '```');
+		}
+
+		const name = args[0].toLowerCase();
+		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+		
+		if (!command) {
+			message.reply(`, that's not a valid command`);
+		}
+
+		const table = new AsciiTable(command.name)
+		table.setHeading('Aliases', 'Description', 'Usage')
+			.addRow(command.aliases, command.description, command.usage);
+		return message.channel.send('```' + table.toString() + '```');
+	}
+};
+
+/*module.exports = {
 	name: 'help',
 	description: 'List all of my commands or info about a specific command.',
 	aliases: ['commands'],
@@ -44,4 +77,4 @@ module.exports = {
 
 		message.channel.send(data, { split: true });
 	},
-};
+};*/
