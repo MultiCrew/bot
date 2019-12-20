4c1894b166b28ea5be5b1d38a719686c10073493
Harry Cameron <hcphoon01@gmail.com>
2019-12-20 21:53:54 +0800

    added ability to have role specific commands, added command disabled option and added announcement command

--- commands/test.js
+++ commands/test.js
@@ -4,27 +4,6 @@ const config = require('config')
 module.exports = {
     name:'test',
     execute(message, args) {
-        var data = {
-            'grant_type': 'client_credentials',
-            'client_id': 3,
-            'client_secret': config.get('auth.secret')
-        }
-        axios.post('http://homestead.test/oauth/token', data)
-        .then(function(response) {
-            token = response.data['access_token'];
-            const options = {
-                headers: {
-                    'Accept':'application/json',
-                    'Authorization': 'Bearer ' + token
-                }
-            }
-            axios.get('http://homestead.test/api/search', options)
-            .then(function(response) {
-                console.log(response.data)
-            })
-            .catch(error => {
-                console.log(error.response)
-            });
-          });
+        message.reply('it worked');
     }
 }
