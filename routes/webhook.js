5db6a8a64ce8c1270b2a8b0f17b9eff2988a0948
Harry Cameron <hcphoon01@gmail.com>
2020-03-26 12:05:40 +0000

    updated webhook route

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,16 +1,25 @@
 const { Route } = require('klasa-dashboard-hooks');
+const querystring = require('querystring');
 
 module.exports = class extends Route {
 
     constructor(...args) {
         super(...args, {
-            route: '/webhook',
+            route: 'webhook',
             authenticated: false
         });
     }
 
     post(request, response) {
-        console.log(request);
-        return response.end(request);
+        const parsed = querystring.parse(request.body);
+        switch (parsed.type) {
+            case 'notification':
+                break;
+            case 'role':
+                break;
+            default:
+                break;
+        }
+        return response.end();
     }
 };
