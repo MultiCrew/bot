0116632196282dc13a835065a82bd1d721dc4c41
Harry Cameron <hcphoon01@gmail.com>
2020-09-26 19:49:15 +0100

    set up webhook listener

--- listeners/webhook.js
+++ listeners/webhook.js
@@ -4,12 +4,38 @@ class WebhookListener extends Listener {
     constructor() {
         super('webhook', {
             event: 'webhook',
-            emitter: 'process'
+            emitter: process
         });
     }
 
     exec(data) {
-        console.log(data);
+        switch (data.type) {
+            case 'notification':
+                this.client.users.fetch(data.id).then(user => {
+                    user.send(data.message);
+                });
+                break;
+            case 'role':
+                break;
+            case 'connection':
+                this.client.users.fetch(data.id).then(user => {
+                    const guild = this.client.guilds.cache.get('440545668168286249');
+                    for (let i = 0; i < data.optional.length; i++) {
+                        const id = data.optional[i];
+                        if(id == '440550777912819712') { continue; }
+                        let role = guild.roles.cache.get(id);
+                        const member = guild.member(user);
+                        member.roles.add(role);
+                    }
+                    user.send(data.message);
+                });
+                break;
+            case 'beta_notification':
+                this.client.channels.cache.get('741561495699062865').send(data.message);
+                break;
+            default:
+                break;
+        }
     }
 }
 
