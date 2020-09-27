4651762ea9b66c6a3933a71820c556740b9f212e
Harry Cameron <hcphoon01@gmail.com>
2020-09-27 16:17:00 +0100

    tiding up index file

--- index.js
+++ index.js
@@ -28,30 +28,6 @@ class MyClient extends AkairoClient {
 		
 		this.commandHandler.loadAll();
         this.listenerHandler.loadAll();
-
-        // this.commandHandler.resolver.addType('aircraftICAO', (message, phrase) => {
-        //     if (!phrase) return null;
-        //     const regex = /[A-Z]{1,}[0-9]{1,}[A-Z]?/;
-
-        //     const result = regex.exec(arg);
-        //     if (result) {
-        //         return arg.toUpperCase();
-        //     } else {
-        //         return null;
-        //     }
-        // });
-
-        // this.commandHandler.resolver.addType('airportICAO', (message, phrase) => {
-        //     if (!phrase) return null;
-        //     const regex = /[A-Z]{4}/i;
-
-        //     const result = regex.exec(arg);
-        //     if (result) {
-        //         return arg.toUpperCase();
-        //     } else {
-        //         return null;
-        //     }
-		// });
     }
 }
 
